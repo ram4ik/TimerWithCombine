010cdf8fdbc975688c6b686076b5985e20809958
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-20 09:22:08 +0300

    init

--- TimerWithCombine/ContentView.swift
+++ TimerWithCombine/ContentView.swift
@@ -6,11 +6,60 @@
 //
 
 import SwiftUI
+import Combine
 
+private var subscription: AnyCancellable?
 struct ContentView: View {
+    @State private var countLabel = 0
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(countLabel)")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+            HStack {
+                Button(action: {
+                    startTimer()
+                }, label: {
+                    Text("Start")
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding()
+                })
+                Button(action: {
+                    stopTimer()
+                }, label: {
+                    Text("Stop")
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding()
+                })
+            }
+        }
+    }
+    
+    private func startTimer() {
+        subscription = Timer
+            .publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .scan(0, { count, _ in
+                return count + 1
+            })
+            .sink(receiveCompletion: { _ in
+                print("Finished")
+            }, receiveValue: { count in
+                countLabel = count
+            })
+    }
+    
+    private func stopTimer() {
+        countLabel = 0
+        subscription?.cancel()
     }
 }
 
