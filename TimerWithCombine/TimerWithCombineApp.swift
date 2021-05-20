6be864c684bd95d29897b165ac4fd3e794eb6d08
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-20 09:08:02 +0300

    Initial Commit

---
+++ TimerWithCombine/TimerWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerWithCombineApp.swift
+//  TimerWithCombine
+//
+//  Created by ramil on 20.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct TimerWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
